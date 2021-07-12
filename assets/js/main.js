bbf6dba86016754eddd6a274a205316231780623
SMRsaimon <smrsaimon960@gmail.com>
2021-07-11 22:04:39 -0700

    ff

--- assets/js/main.js
+++ assets/js/main.js
@@ -380,23 +380,25 @@ function initMap() {
 
   // intregate messenger 
 
-    var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "279931489281757");
-      chatbox.setAttribute("attribution", "biz_inbox");
+   
 
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v11.0'
-        });
-      };
+      var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "279931489281757");
+  chatbox.setAttribute("attribution", "biz_inbox");
 
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v11.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
       
